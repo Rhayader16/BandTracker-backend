504f82eaf01526fb222eeee4652ad6cea3b5d080
Roberta <robyfranky@hotmail.com>
2023-11-17 12:43:47 +0100

    minor changes

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("./../models/User.model");
+
 const isAuthenticated = async (req, res, next) => {
   try {
     let token = req.headers.authorization;
